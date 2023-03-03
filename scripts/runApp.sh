d0af3dacb0250cb02bd49f24a41c16f4a95c5452
yvalcorp <yvalcorp@gmail.com>
2023-03-02 21:34:33 -0500

    edited the runApp.sh file

--- scripts/runApp.sh
+++ scripts/runApp.sh
@@ -5,10 +5,10 @@
 echo 'The following "npm" command runs your Node.js application'
 
 set -x
-npm start &
-sleep 1
-echo $! > .pidfile
+npm start &  # & means run the process on the backgroup 
+sleep 1      # Pause for 1 second
+echo $! > .pidfile    # Pass process ID 
 set +x
 
 echo 'Now you can'
-echo 'Visit http://ServerIp:9981 to see your Node.js application'
+echo 'Visit http://192.168.56.220:9981 to see your Node.js application'
